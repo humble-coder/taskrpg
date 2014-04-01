489c08f071b1817670193eedf985d0bfdcfc79eb
Mark A Brown <mark.philosophe@gmail.com>
2014-03-31 23:28:33 -0400

    established rails foundation

---
+++ taskrpg/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Taskrpg::Application.config.secret_key_base = 'fa0d3b8780f2b56d22faf1cbe9ea5c7ce0271352b95df2509edfeb1b7d0a53d003c2beb558da004275ed0fc00bd604ec8fa338ea5284db63b6a2b025dcad0c03'
