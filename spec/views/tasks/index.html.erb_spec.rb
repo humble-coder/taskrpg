699d80defdd3178e29554204e2dedd1def73c102
Mark A Brown <mark.philosophe@gmail.com>
2014-04-06 12:19:18 -0400

    updated index-view spec

--- spec/views/tasks/index.html.erb_spec.rb
+++ spec/views/tasks/index.html.erb_spec.rb
@@ -9,15 +9,20 @@ describe "tasks/index" do
       ),
       stub_model(Task,
         :name => "MyText",
-        :priority => 1
+        :priority => 2
       )
     ])
   end
 
   it "renders a list of tasks" do
     render
+
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    # assert_select "tr>td", :text => "MyText".to_s, :count => 2
-    # assert_select "tr>td", :text => 1.to_s, :count => 2
+    assert_select "ul" do |elements|
+      elements.each do |element|
+        assert_select element, "li", 2
+      end
+    end
+    
   end
 end
