5d88b01e65042d97a876c2bc70d5aa9addd50142
Mark A Brown <mark.philosophe@gmail.com>
2014-05-10 11:23:29 -0400

    added mailer spec

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,5 +1,17 @@
 require "spec_helper"
 
 describe UserMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	let(:user) { create :user }
+  let(:email) { UserMailer.password_reset(user).deliver }
+
+  it "has a from address of admin@task.rpg" do
+  	expect(email.to).to eq([user.email])
+  	expect(email.from).to eq(["admin@taskrpg.com"])
+  	expect(email.subject).to eq("Password Reset")
+  	expect(ActionMailer::Base.deliveries.empty?).to eq(false)
+  	expect(email.body).to have_content(user.name)
+  	expect(email.body).to have_content(user.password)
+  end
+
 end
